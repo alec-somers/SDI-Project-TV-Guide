abc5cc8880e787cc741ba74750680b247f87104c
alec-somers <alecv1528@gmail.com>
2025-07-18 15:15:38 -0700

    Add form submission handling and enhance show display styling

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,19 @@
+const submitForm = document.querySelector(".main-form");
+submitForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  console.log(event);
+  console.log(document.querySelector(".searchbar").value);
+});
+
 function displayTopFiveShows(shows) {
   for (let i = 0; i < 5; i++) {
     let showName = shows[i].name;
+    let showSummary = shows[i].summary;
     let newLi = document.createElement("li");
-    newLi.innerHTML = `<img class="show-image" src=${shows[i].image.medium}></img><p>Try ${showName}!</p>`;
-    newLi.setAttribute("class", "top-movies-item");
+    newLi.innerHTML = `<img class="show-image" src=${shows[i].image.medium}></img>
+    <div class="text-container"><p>Try ${showName}!</p>
+    ${showSummary}</div>`;
+    newLi.setAttribute("class", "top-show-item flex");
     document.querySelector(".movie-list").appendChild(newLi);
   }
 }
